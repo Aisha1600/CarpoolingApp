517e25e6f9e729219d4ea58b7ad6429e25efe0fa
Aksay17 <68349469+Aksay17@users.noreply.github.com>
2023-02-18 19:28:52 +0500

    please check these APIs ayesha

--- server/index.js
+++ server/index.js
@@ -3,12 +3,20 @@ const app = express();
 const cors = require("cors");
 const pool = require("./db");
 const car_route= require('./routers/car_route')
-const member_route= require('./routers/member_route')
+const member_route= require('./routers/member_router')
+const destination_route= require('./routers/destination_route')
+const membercar_route= require('./routers/membercar_router')
+
 
 
 app.use(cors());
 app.use(express.json())
 app.use('/car',car_route)
+app.use('/member',member_route)
+app.use('/membercar',membercar_route)
+app.use('/destination',destination_route)
+
+
 
 
   
