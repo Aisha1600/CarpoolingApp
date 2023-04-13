8ebf702677c629a78db7a4c0d1ee3ee2d0e21e8f
Aksay17 <68349469+Aksay17@users.noreply.github.com>
2023-04-13 22:06:11 +0500

    made more apis for preference

--- server/controllers/pref_controller.js
+++ server/controllers/pref_controller.js
@@ -1,10 +1,30 @@
 const pool = require("../db");
 
 module.exports={
-    //user adds a preference
+
+    //changes made
+    //working!
+  AddPrefer: async (req, res) => {
+    try {
+        const { member_id } = req.params;
+        const { is_smoking_allowed, is_all_female, notes } = req.body;
+        await pool.query('INSERT INTO preference (member_id, is_smoking_allowed, is_all_female, notes) VALUES ($1, $2, $3, $4) RETURNING *',
+            [member_id, is_smoking_allowed, is_all_female, notes]);
+        res.status(201).send('Preference added successfully!');
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send('Server Error');
+    }
+},
+
+    
+    //user adds a preference (Aamna)
+    //not working properly
     AddPref:  async (req, res) => {
         
         try {
+        //  const { member_id } = req.params;
+
            const {is_smoking_allowed, is_all_female, notes} = req.body;
           await pool.query('INSERT INTO preference (is_smoking_allowed, is_all_female, notes) VALUES ($1, $2, $3) RETURNING *',
           [is_smoking_allowed, is_all_female, notes]);
@@ -16,6 +36,7 @@ module.exports={
         }
       },
       //user deletes a preference
+      //working!!
       DeletePref: async (req, res) => {
         try {
             const { preference_id } = req.body;
@@ -28,4 +49,34 @@ module.exports={
           res.status(500).send('Error deleting preference.');
         }
       },      
+      //needed this for update/edit
+      //working
+      UpdatePrefForMember: async (req, res) => {
+        try {
+          const { member_id, is_smoking_allowed, is_all_female, notes } = req.body;
+          const updatePref = await pool.query(
+            'UPDATE preference SET is_smoking_allowed = $1, is_all_female = $2, notes = $3 WHERE member_id = $4',
+            [is_smoking_allowed, is_all_female, notes, member_id]
+          );
+          res.json(`Preferences updated for member with ID ${member_id}.`);
+        } catch (err) {
+          console.error(err);
+          res.status(500).send('Error updating preferences.');
+        }
+      },
+      // Get preferences for a member
+GetPref: async (req, res) => {
+  try {
+    const member_id = req.params.member_id;
+    const getpref = await pool.query(
+      'SELECT * FROM preference WHERE member_id = $1', [member_id]
+    );
+    res.json(getpref.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error retrieving preferences.');
+  }
+}
+
+      
 }
--- server/routers/perf_route.js
+++ server/routers/perf_route.js
@@ -2,8 +2,11 @@ const pool = require('../db.js');
 const router = require('express').Router();
 const pref_controller = require("../controllers/pref_controller");
 
-router.post('/AddPreference', pref_controller.AddPref)
-router.delete('/DeletePreference', pref_controller.DeletePref)
+router.post('/member/:member_id/preference', pref_controller.AddPrefer)
+router.delete('/DeletePreference/:preference_id', pref_controller.DeletePref)
+router.post('/member/:member_id/preference/update', pref_controller.UpdatePrefForMember);
+router.get('/member/:member_id/preference', pref_controller.GetPref);
+
 
 
 module.exports = router;
